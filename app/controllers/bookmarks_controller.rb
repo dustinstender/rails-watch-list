4508c095c9a6da4dd54edbc55cf3b0a4d8b6e324
Dustin Stender <dustinpstender@gmail.com>
2021-08-13 15:48:19 +0200

    hopefully finished the photo upload part

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -23,6 +23,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :photo)
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_one_attached :photo
   validates_uniqueness_of :movie, scope: :list
 
   validates :comment, length: { minimum: 6 }
